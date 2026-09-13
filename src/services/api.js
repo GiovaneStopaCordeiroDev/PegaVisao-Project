9fb0c23a8df0539ec9bd5ecb6dc11395ba866af4
unknown <giovanestopacordeirodev@gmail.com>
2026-09-13 20:22:51 -0300

    Trocando baseURL para Api Render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://localhost:7263"
+    baseURL: "https://pegavisao-project-api.onrender.com"
 });
 
 export default api;
